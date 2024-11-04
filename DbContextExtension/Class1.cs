f00e4d7f1240a5f4ac1c71f0d289758bb751f977
robin <alegalone@gmail.com>
2024-11-04 15:09:47 +0800

    不知道這樣可不可以，還沒實驗過

---
+++ DbContextExtension/Class1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+namespace DbContextExtension
+{
+    public interface HasId
+    {
+        string Id { get; set; }
+    }
+
+    public class DbSetEntity<TEntity> : DbSetUnikey<TEntity> where TEntity : class, HasId
+    {
+        private readonly DbSet<TEntity> _dbSet;
+
+        public DbSetEntity(DbSet<TEntity> dbSet) : base(dbSet, x => y => x.Id == y.Id)
+        {
+            this._dbSet = dbSet;
+        }
+    }
+    public class DbSetUnikey<TEntity> : DbSet<TEntity> where TEntity : class
+    {
+        private readonly DbSet<TEntity> _dbSet;
+
+        protected Func<TEntity, Expression<Func<TEntity, bool>>> findByUnikey;
+        public override IEntityType EntityType => this._dbSet.EntityType;
+        public DbSetUnikey(DbSet<TEntity> dbSet, Func<TEntity, Expression<Func<TEntity, bool>>> unikeyMapper)
+        {
+            this._dbSet = dbSet;
+            this.findByUnikey = unikeyMapper;
+        }
+
+        public TEntity FindByUnikey(TEntity data)
+        {
+            var entity = this._dbSet.First(this.findByUnikey(data));
+            return entity;
+        }
+    }
+    public class DbSetWithCache<TEntity> : DbSet<TEntity> where TEntity : class
+    {
+        private readonly DbSet<TEntity> _dbSet;
+        private readonly ConcurrentDictionary<string, TEntity> _cache;
+
+        private readonly Func<TEntity, string> getKey;
+        public DbSetWithCache(DbSet<TEntity> dbSet, Func<TEntity, string> getIdFunc)
+        {
+            this._dbSet = dbSet;
+            this._cache = new ConcurrentDictionary<string, TEntity>();
+            this.getKey = getIdFunc;
+        }
+
+        public override IEntityType EntityType => this._dbSet.EntityType;
+
+        // 包裝 Add 方法，將實體添加到字典中
+        public override EntityEntry<TEntity> Add(TEntity entity)
+        {
+            var key = this.getKey(entity);
+            this._cache.TryAdd(key, entity);
+            return this._dbSet.Add(entity);
+        }
+
+        // 查詢方法，先查字典再查資料庫
+        public TEntity FindById(string id)
+        {
+            if (this._cache.TryGetValue(id, out TEntity cachedEntity))
+            {
+                return cachedEntity;
+            }
+
+            var entity = this._dbSet.Find(id);
+            return entity;
+        }
+
+    }
+}
